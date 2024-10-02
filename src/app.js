2ae4ad58914f4d818604095fa9cde0eae91e7f74
CRVanware <crodriguez@vanware.io>
2024-10-02 19:04:58 +0000

    proyecto def

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+  let var1 = Math.floor(Math.random() * who.length);
+  let var2 = Math.floor(Math.random() * action.length);
+  let var3 = Math.floor(Math.random() * what.length);
+  let var4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[var1] + action[var2] + what[var3] + when[var4];
 };
